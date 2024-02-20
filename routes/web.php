29140cc6761be9f35c01c5c49b1bee0d819209ed
Earthweb <107529899+Earthwweb@users.noreply.github.com>
2024-02-21 00:35:51 +0545

    category resource added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group([
+        'prefix'  => 'backend',
+        'middleware' => ['auth']
+    ],
+    function () {
+        Route::resource('category', CategoryController::class)->names('backend.category');
+});
